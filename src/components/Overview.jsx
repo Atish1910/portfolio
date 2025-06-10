c827400ee77270c1d00a3188ee62f00d53969381
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2025-06-10 12:58:31 +0530

    21

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-// import statImg from "../assets/img/stats/";
+import statImg1 from "/assets/img/stats/1.png";
+import statImg2 from "/assets/img/stats/2.png";
+import statImg3 from "/assets/img/stats/3.png";
+import statImg4 from "/assets/img/stats/4.png";
 
 const Overview = () => {
   const stats = [
-    { img: "1", num: "70", text: "Projects Completed" },
-    { img: "2", num: "5", text: "Custom Admin Panels" },
-    { img: "3", num: "100,000", text: "Lines of Code" },
-    { img: "4", num: "2.5", text: "Years of Development Experience" },
+    { img: statImg1, num: "70", text: "Projects Completed" },
+    { img: statImg2, num: "5", text: "Custom Admin Panels" },
+    { img: statImg3, num: "100,000", text: "Lines of Code" },
+    { img: statImg4, num: "2.5", text: "Years of Development Experience" },
   ];
   return (
     <>
@@ -18,11 +21,7 @@ const Overview = () => {
                 <div className=" col-lg-3  d-lg-grid mb-3 " key={stat.text}>
                   <div className=" usp_01 py-3 bg-black rounded-3">
                     <div className=" text-center">
-                      <img
-                        src={`/assets/img/stats/${stat.img}.png`}
-                        alt=""
-                        className="stats"
-                      />
+                      <img src={stat.img} alt="" className="stats" />
                       <h6 className=" rank text-white mb-0 fw-bold  pt-3 h3">
                         {stat.num}
                         <span className="plus">+</span>
